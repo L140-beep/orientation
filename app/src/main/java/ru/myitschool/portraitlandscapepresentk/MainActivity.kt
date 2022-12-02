28e68fd74cfbb7b7b2e4035f0ed7923b49cc19a6
Ivan Petr <ivan.kiel@gmail.com>
2022-12-02 13:02:08 +0800

    TODO added, images added

--- app/src/main/java/ru/myitschool/portraitlandscapepresentk/MainActivity.kt
+++ app/src/main/java/ru/myitschool/portraitlandscapepresentk/MainActivity.kt
@@ -2,10 +2,22 @@ package ru.myitschool.portraitlandscapepresentk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // TODO: заменять картинку на ту, которую установили при нажатии кнопки
+        // иначе будет исходная с котиком
+    }
+
+    fun onChangePictureClick(v: View) {
+        val iv = findViewById<ImageView>(R.id.picture)
+
+        // TODO: картинку менять по очереди на следущую
+        iv.setImageResource(R.drawable.car1)
+
     }
 }
