caaa5df324fca5eff9be5d8378b5cadd5b5c377f
L140-beep <kartoshka0302@mail.ru>
2022-12-27 15:17:52 +0800

    complete

--- app/src/main/java/ru/myitschool/portraitlandscapepresentk/MainActivity.kt
+++ app/src/main/java/ru/myitschool/portraitlandscapepresentk/MainActivity.kt
@@ -1,23 +1,37 @@
 package ru.myitschool.portraitlandscapepresentk
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    var curPicture : Int = -1
+    lateinit var pictures : List<Int>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // TODO: заменять картинку на ту, которую установили при нажатии кнопки
-        // иначе будет исходная с котиком
+        var iv = findViewById<ImageView>(R.id.picture)
+        pictures = listOf(R.drawable.car1, R.drawable.car2, R.drawable.car3)
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle ) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("counter", curPicture)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        var iv = findViewById<ImageView>(R.id.picture)
+        curPicture = savedInstanceState.getInt("counter")
+        iv.setImageResource(pictures[curPicture % pictures.size])
     }
 
     fun onChangePictureClick(v: View) {
-        val iv = findViewById<ImageView>(R.id.picture)
-
-        // TODO: картинку менять по очереди на следущую
-        iv.setImageResource(R.drawable.car1)
-
+        var iv = findViewById<ImageView>(R.id.picture)
+        curPicture += 1
+        iv.setImageResource(pictures[curPicture % pictures.size])
     }
 }
